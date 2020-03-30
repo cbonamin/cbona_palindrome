657119ed1517d7ff09c481175ee356185d5c182c
cbonamin <cbonamin@gmail.com>
2020-03-30 10:53:19 -0300

    Solve bug

--- lib/cbona_palindrome.rb
+++ lib/cbona_palindrome.rb
@@ -1,6 +1,28 @@
 require "cbona_palindrome/version"
 
 module CbonaPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      to_s.scan(/[a-zA-Z0-9]/).join.downcase
+    end
+end
+
+class String
+  include CbonaPalindrome
+end
+
+class Integer
+  include CbonaPalindrome
 end
--- test/cbona_palindrome_test.rb
+++ test/cbona_palindrome_test.rb
@@ -1,11 +1,28 @@
 require "test_helper"
 
 class CbonaPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::CbonaPalindrome::VERSION
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
+  def test_integer_non_palindrome
+      refute 12345.to_s.palindrome?
+  end
+
+  def test_integer_palindrome
+      assert 123321.to_s.palindrome?
   end
 end
